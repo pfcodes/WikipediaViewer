5bc71b8a9eff5a4493c53351c7a193499ca659ea
Phil Fevry <phil.fevry@gmail.com>
2017-05-26 21:12:41 -0400

    update to script

--- js/wikipediaviewer.js
+++ js/wikipediaviewer.js
@@ -27,14 +27,14 @@ WikipediaViewer = {
 		s.searchQuery = s.api.url;
 		for (i = 1; i < Object.keys(s.api).length; i++) {
 			s.searchQuery += (Object.keys(s.api)[i] + '=' + s.api[Object.keys(s.api)[i]]);
-			if (i != Object.keys(s.api).length-1) 
-				s.searchQuery += '&';
+			if (i != Object.keys(s.api).length-1) s.searchQuery += '&';
 		}	
 	},
 
 	bindEvents: function() {
 		// when search button is clicked
 		s.searchButton.on('click', function() {
+			WikipediaViewer.refreshQueryParameters();
 			s.searchQuery += s.searchBox.val().replace(' ','+');
 			$.getJSON(s.searchQuery, DataParser.interpret);
 			console.log(s.searchQuery);
@@ -44,17 +44,20 @@ WikipediaViewer = {
 
 DataParser = {
 	interpret: function(data) {
-		var title, description, url;
-		//title = data.query.normalized.to;
-		//description = data[0];
-		//DOMUpdater.addElement('<b>' + data.title + '</b>');
-		//DOMUpdater.addElement(data.description);
+		var item;
+		item = data.query.search;
+		$('#searchResults').empty();
+		for (i = 0; i < item.length; i ++) {
+			current = item[i];
+			DOMUpdater.addElement('<b>' + current.title + '</b>');
+			DOMUpdater.addElement(current.snippet);
+		}
 	}
 };
 
 DOMUpdater = {
 	addElement: function(data) {
-		$('<p>' + data + '</p>').appendTo('.container-fluid');
+		$('<p>' + data + '</p>').appendTo('#searchResults');
 	},
 };
 
